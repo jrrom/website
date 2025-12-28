9bbd520187e0fe45b060d34f7176af57327656a9
jrrom <77691121+jrrom@users.noreply.github.com>
2025-12-28 14:57:45 +0530

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "jrrom.com dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self , nixpkgs ,... }:
+    let
+      system = "x86_64-linux";
+    in
+      {
+        devShells."${system}".default = let
+          pkgs = import nixpkgs { inherit system; };
+        in pkgs.mkShell {
+          # Create an environment with Racket
+          packages = with pkgs; [
+            (ghc.withPackages(ps: with ps; [
+              cabal-install
+              haskell-language-server
+            ]))
+            zlib
+          ];
+        };
+      };
+}
+
